2fbc049532557b7eb6f2e30b67e47cf60eb8d7dc
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-10-11 22:31:11 +0200

    Try move model

--- services/ClusterMetaDataService.py
+++ services/ClusterMetaDataService.py
@@ -4,7 +4,20 @@ import yaml
 import urllib
 import urllib.request
 
-from models.Cluster import Cluster
+
+class Cluster:
+    display_name = ""
+    name = ""
+    password = ""
+    file_name = ""
+    server = ""
+
+    def __init__(self, display_name, name, file_name, server, password=""):
+        self.display_name = display_name
+        self.name = name
+        self.file_name = file_name
+        self.password = password
+        self.server = server
 
 
 class ClusterMetaDataService:

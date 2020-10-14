a1ab2b90895e7a3e7abfd43fb251730f4113944c
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-10-14 02:29:41 +0200

    End UT

--- tests/test_ClusterMetaDataService.py
+++ tests/test_ClusterMetaDataService.py
@@ -1,1 +1,49 @@
+import pytest
+
 from Services.ClusterMetaDataService import ClusterMetaDataService
+
+
+def test_read_from_file(mocker):
+
+    mock_open = mocker.mock_open(read_data="""
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: LS0tL
+    server: https://127.0.0.1:6443
+  name: default
+contexts:
+- context:
+    cluster: default
+    user: default
+  name: default
+current-context: default
+    """)
+
+    mocker.patch("Services.ClusterMetaDataService.open", mock_open)
+    
+    service = ClusterMetaDataService("./test")
+
+    cluster = service.read_from_file("./test.file")
+
+    assert cluster.name == "default"
+    assert cluster.display_name == "default"
+    assert cluster.file_name == "test.file"
+    assert cluster.server == "https://127.0.0.1:6443"
+
+def test_read_from_file_error(mocker):
+
+    mock_open = mocker.mock_open(read_data="""
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: LS0tL
+    server: https://127.0.0.1:6443
+  name: default
+    """)
+
+    mocker.patch("Services.ClusterMetaDataService.open", mock_open)
+    
+    service = ClusterMetaDataService("./test")
+    with pytest.raises(Exception):
+        service.read_from_file("./test.file")
--- tests/test_PathService.py
+++ tests/test_PathService.py
@@ -15,28 +15,28 @@ def test_get_no_windows_url(mocker):
 
 
 def test_get_working_directory(mocker):
-    mocker.patch('pathlib.Path.home', return_value="c:\\")
+    mocker.patch('pathlib.Path.home', return_value="c:/")
     path = get_working_directory()
 
-    assert path == "c:\\.pykubeswitch"
+    assert path == "c:/.pykubeswitch"
 
 
 def test_get_kube_directory(mocker):
-    mocker.patch('pathlib.Path.home', return_value="c:\\")
+    mocker.patch('pathlib.Path.home', return_value="c:/")
     path = get_kube_directory()
 
-    assert path == "c:\\.kube"
+    assert path == "c:/.kube"
 
 
 def test_find_available_name(mocker):
     mocker.patch("os.path.isfile", return_value=False)
-    path = find_available_name("c:\\test", "file")
+    path = find_available_name("c:/test", "file")
 
     assert path == "file"
 
 
 def test_find_available_name_2_iterations(mocker):
     mocker.patch("os.path.isfile", side_effect=[True, True, False])
-    path = find_available_name("c:\\test", "file")
+    path = find_available_name("c:/test", "file")
 
     assert path == "file_2"
